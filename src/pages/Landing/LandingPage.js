005d0d74b577bf4f6a742fcec598320666ee1fa5
pee47kay <papakowdadson@gmail.com>
2025-03-23 00:23:56 +0000

    made changes

--- src/pages/Landing/LandingPage.js
+++ src/pages/Landing/LandingPage.js
@@ -249,8 +249,9 @@ const ActionsWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
+  flex-wrap: wrap;
   @media (max-width: 540px) {
-    flex-wrap: wrap;
+    flex-direction: column;
   }
 `;
 
